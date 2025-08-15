2e279dc2bb5d459b5bbdd739b00666a27cf82c91
sith <skdev.techno@gmail.com>
2025-08-15 15:31:13 +0530

    feat: add post entity

---
+++ backend/src/main/java/com/hackathon_hub/hackathon_hub_api/entity/Post.java
@@ -0,0 +1,38 @@
+package com.hackathon_hub.hackathon_hub_api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Post {
+
+    @Id
+    @GeneratedValue
+    private UUID id; // UUID primary key
+
+    private String caption;
+
+    private String imagePath;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by_id")
+    private User createdBy; // relationship with User entity
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    private boolean approved = true; // default false for future moderation
+}
