54b247c5f936a53c5328b99cb0f57d7d3cdf9e60
sith <skdev.techno@gmail.com>
2025-08-26 14:13:15 +0530

    chore: change security config for testing

--- src/main/java/com/hackathon_hub/hackathon_hub_api/config/SecurityConfig.java
+++ src/main/java/com/hackathon_hub/hackathon_hub_api/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/").permitAll()
                         .requestMatchers("/api/v1/auth/add-user").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/api/v1/task").hasRole("ADMIN")
                         .requestMatchers("/api/v1/auth/**").permitAll()
@@ -57,7 +58,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+//        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
--- src/main/java/com/hackathon_hub/hackathon_hub_api/config/WebConfig.java
+++ src/main/java/com/hackathon_hub/hackathon_hub_api/config/WebConfig.java
@@ -1,24 +1,16 @@
 package com.hackathon_hub.hackathon_hub_api.config;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Serve uploaded files from "uploads" folder
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
-    }
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-
-
     }
 }
