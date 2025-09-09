45b008419c59ff27ac370abc6ed6222e71769798
sith <skdev.techno@gmail.com>
2025-09-09 16:22:51 +0530

    config: add allowed origins

--- src/main/java/com/hackathon_hub/hackathon_hub_api/config/SecurityConfig.java
+++ src/main/java/com/hackathon_hub/hackathon_hub_api/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://hackathon-hub-ui.vercel.app"));
         configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
